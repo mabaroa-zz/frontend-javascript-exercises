2c40be254aef911557a1db2ee88cf3919abae612
Miguel Abaroa <mabaroa@gmail.com>
2014-09-15 21:38:01 +0000

    passed all tests

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,31 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title,duration,students){
+ return {
+  title: title,
+  duration: duration,
+  students: students
+ };
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object,newProp,newValue){
+  if (object[newProp] == undefined) {
+    object[newProp]= newValue;
+   return object;
+}
+ else {
+   return object;
+  }
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+return "Hello " + letter.recipient + "," + "\n\n" + letter.msg  + "\n\n" + "Sincerely," + "\n" +letter.sender
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var priceChart = {
+   "Mac Pro": 2499,
+   "MacBook Pro": 1299,
+   "MacBook Air": 999,
+   "Apple Sticker": 1
+  }
+  return priceChart[item] <= money;
+  };

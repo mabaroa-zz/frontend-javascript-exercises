499fec884a431855098a4c09425a491fdefb2744
Miguel Abaroa <mabaroa@gmail.com>
2014-09-15 14:18:30 +0000

    passed test

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,7 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a,b,c,d) {
+ return [a,b,c,d];
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array){
+return [array[0] , array[array.length - 1]];
+};

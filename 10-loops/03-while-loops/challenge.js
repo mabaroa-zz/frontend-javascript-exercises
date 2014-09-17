267cb818259b9c22d612305caf0469822f4d6dcb
Miguel Abaroa <mabaroa@gmail.com>
2014-09-17 03:15:06 +0000

    passed all tests

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,16 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn){
+  while(conditionalFn() == true){
+    actionFn()
+  }
+}
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function(array){
+  var sum = 0
+  while(array != 0){
+    for (var counter = 0; counter < array.length; counter ++)  {
+      sum = sum + array[counter]
+    }
+    return sum
+  }
+  return 0
+}

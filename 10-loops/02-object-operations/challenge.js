6ead4e27325b6a253fb477629630f3e6cda3e4d7
Miguel Abaroa <mabaroa@gmail.com>
2014-09-17 03:05:22 +0000

    passed all tests

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,23 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object){
+var newObject = {};
+for (var key in object){
+  newObject[key] = object[key]
+}
+return newObject;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, srce){
+  for (var key in srce){
+    dest[key] = srce[key];
+  }
+  return dest;
+ };
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, arrayOfElements){
+  for (var x=0; x < arrayOfElements.length; x ++){
+    if(!object.hasOwnProperty(arrayOfElements[x])) {
+      return false;
+    }
+    }
+        return true;
+  };
